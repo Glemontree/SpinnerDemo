94c9a7f461c8a304064f4539d9d193afdf7eda84
gtp <gtp0521@gmail.com>
2016-08-20 21:53:47 +0800

    first commit

---
+++ src/com/example/spinnerdemo/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.spinnerdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	private Spinner spinner;
+	private TextView textView;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		spinner = (Spinner) findViewById(R.id.spinner);
+		textView = (TextView) findViewById(R.id.text);
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {
+				// TODO Auto-generated method stub
+				String[] cities = getResources().getStringArray(R.array.city_labels);
+				textView.setText("您喜欢的城市是：" + cities[position]);
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
